c4bf1bbdae6ef8d4873569c8cbf6e2f267992562
Aqib Javed <aqibbutt3078@gmail.com>
2020-06-28 22:57:45 +0500

    Evs Batch 49 lecture 1

---
+++ EvsBatch49Lect1/src/MainClass.java
@@ -0,0 +1,36 @@
+
+
+// class pascal case
+public class MainClass {
+
+//	public static void main(String[] args) {
+//		//Console.writeLine("Hi every one"); //C#
+//		
+//	}
+	
+	// camecase  -> calculatePercentage 
+	
+	public static void main(String[] args) {
+		// Preemptive
+	// 2^8 -> -128 -> -1, 0, 1 -> 127 
+		byte b = 127; // 8 bits
+		short c = 129; // 16 bits
+		// 
+		int x = 1234; // 32 bits
+		
+		//
+		long y = 172920; // 64 bits
+		
+		boolean xk = true; // 1 bit 
+		
+		char charac = 'c'; // 2 bytes -> 16 bits UTF-8
+		
+		float ff = 2.3f; // 32 bits -> decimal . 7 allow
+		double dd = 3.456; // 64 bits -> decimal . 16 allow
+		
+		System.out.println("Hi every one");
+		
+		
+	}
+
+}
