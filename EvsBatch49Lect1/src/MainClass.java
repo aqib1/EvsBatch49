99785be62899e39051e24f167da8fdd6c1a1d2e4
Aqib Javed <aqibbutt3078@gmail.com>
2020-07-04 18:08:25 +0500

    String, immutatble class, string pool

--- EvsBatch49Lect1/src/MainClass.java
+++ EvsBatch49Lect1/src/MainClass.java
@@ -1,5 +1,3 @@
-
-
 // class pascal case
 public class MainClass {
 
@@ -11,24 +9,139 @@ public class MainClass {
 	// camecase  -> calculatePercentage 
 	
 	public static void main(String[] args) {
+		// ABSGWFHWGV false
+		// ABCDEFGHIJK true
+		
+		//cout<<sizeof(&x);
+		
+//		String s = new String("ABC");
+//		String s1 = "ABCBNABC";
+//		s1 = s1.toLowerCase();
+//		System.out.println(s1.toLowerCase());
+//		System.out.println(s1.toUpperCase());
+//		System.out.println(s1.charAt(0));
+//		System.out.println(s1.concat( " DEF"));
+//		System.out.println(s1.contains("C"));
+//		System.out.println(s1.endsWith("AABC"));
+//		System.out.println(s1.trim());
+//		System.out.println(s1.indexOf("B"));
+//		char [] arr = s1.toCharArray();
+//		for(int x=0; x< arr.length; x++) {
+//			System.out.println(arr[x]);
+//		}
+//		System.out.println(s1.indexOf("Z"));
+//		System.out.println(s1.indexOf("B", 4));
+//		System.out.println(s1.isEmpty());
+//		System.out.println(s1.length());
+//		System.out.println(s1.replace('B', 'Z'));
+//		System.out.println(s1.replaceAll("ABC", "CAT"));
+		
+//		String s1 = "ABCBNABC";
+//		System.out.println(s1.substring(2));
+//		System.out.println(s1.substring(2, s1.length()));// i2c
+//		System.out.println((int)s1.charAt(0));
+//		s1.chars() //
+//		.mapToObj(x -> (char)x) 
+//		.forEach(System.out::println);
+//		s1 = "ABC" +" DEF";
+//		s1 = s1.concat(" DEF");
+		
+		
+		
+//		System.out.println(s.toLowerCase().equals(s1.toLowerCase()));
+//		System.out.println(s);
+//		s -> "ABC";
+//		s1 -> "ABC";
+		
+//		Object obj1 = new Object();
+//		Object obj2 = obj1;
+//		System.out.println(obj1 == obj2);
+		
+		
+		// Task 1
+		// we have given an array where, one element is repeated, find
+		// that and return 
+		// [1, 2, 1, 3]
+		
+		// Task 2
+		// we have an array which is sure, sorted one, 
+		// we have to find a number index of K from it
+		// i.g 
+		// [1, 2, 3, 4 , 5] = find index of 5 algorithm should be log(n)
+		
+		
+		
+		
+		
+//		int [] array = {4, 5, 2, 10, 1};
+//		Arrays.sort(array); // O(n log(n)) > O(n)
+//		System.out.println(array[array.length-1]);
+//		
+//		if(array == null || array.length == 0) {
+//			System.out.println("No elements found");
+//		} else {
+//		// Time complexity O(n-1) and space complexity O(1)
+//		int MAX_VALUE = array[0]; //2^31
+//		for(int  x=1; x<array.length;x++) {
+//			if(MAX_VALUE < array[x]) {
+//				MAX_VALUE = array[x];
+//			}
+//		}
+//		
+//		System.out.println(MAX_VALUE);
+//		}
+//		
+//		
+		
+		
+		
+		
+		
+		
+		
+//		8*4 = 32
+		
+		
+		// {1, 2, 3, 4, 5}
+		// {5, 6, 2, 3, 4}
+		// {}
+		// {1, 2}
+		// {-1, -2, -3} -> -1
+		
+//		int[] ar = new int[] {};
+//		
+//		int[] array = {3, 4, 5} ; //
+//		int[] array1 = new int[] {1,2,3};//
+//		int[] array2 = new int[3];
+//		array2[0] = 1;
+//		array2[1] = 2;
+//		array2[2] = 3;
+		
+//		System.out.println(Arrays.toString(array));
+		
+//		for(int x=0; x<array.length; x++) {
+//			System.out.print(x + " => " +array[x]+"\n");
+//		}
+//		System.out.println();
+		
 		// Preemptive
 	// 2^8 -> -128 -> -1, 0, 1 -> 127 
-		byte b = 127; // 8 bits
-		short c = 129; // 16 bits
-		// 
-		int x = 1234; // 32 bits
-		
-		//
-		long y = 172920; // 64 bits
-		
-		boolean xk = true; // 1 bit 
-		
-		char charac = 'c'; // 2 bytes -> 16 bits UTF-8
-		
-		float ff = 2.3f; // 32 bits -> decimal . 7 allow
-		double dd = 3.456; // 64 bits -> decimal . 16 allow
-		
-		System.out.println("Hi every one");
+//		byte b = 127; // 8 bits
+//		short c = 129; // 16 bits
+//		// 
+//		int x = 1234; // 32 bits
+//		
+//		//
+//		long y = 172920; // 64 bits
+//		
+//		boolean xk = true; // 1 bit 
+//		
+//		char charac = 'c'; // 2 bytes -> 16 bits UTF-8
+//		
+//		float ff = 2.3f; // 32 bits -> decimal . 7 allow
+//		double dd = 3.456; // 64 bits -> decimal . 16 allow
+//		
+//		System.out.println("Hi every one");
 		
 		
 	}
