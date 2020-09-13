31e777844144893c8979b8d5d4e6654229355acb
Aqib Javed <aqibbutt3078@gmail.com>
2020-09-13 16:14:22 +0500

    set on click listeners

--- EvsAndroid49/app/src/main/java/com/evs/evsandroid/evsandroid49/MainActivity.java
+++ EvsAndroid49/app/src/main/java/com/evs/evsandroid/evsandroid49/MainActivity.java
@@ -5,12 +5,33 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private Button ok_btn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initComponents();
+        settingUpListener();
+    }
+
+    private void settingUpListener() {
+        ok_btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Hi there", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initComponents() {
+        ok_btn =  findViewById(R.id.btn_ok);
     }
 
     @Override
@@ -52,4 +73,10 @@ public class MainActivity extends Activity {
     protected void onDestroy() {
         super.onDestroy();
     }
+
+
+//    public void okClick(View v){
+//        Toast.makeText(this, "Hi there", Toast.LENGTH_LONG).show();
+//    }
+
 }
